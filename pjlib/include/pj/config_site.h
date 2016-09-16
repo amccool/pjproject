04321fafdbde200b464c2ae13be4d556ae56ebb7
Alex McCool <alex.mccool@yahoo.com>
2016-09-15 21:32:43 -0700

    build  dlls

--- pjlib/include/pj/config_site.h
+++ pjlib/include/pj/config_site.h
@@ -2,3 +2,16 @@
  *
  *  https://trac.pjsip.org/repos/wiki/Getting-Started/Build-Preparation
  */
+
+
+#define PJ_DLL 1
+#define PJ_EXPORTING 1
+#define PJ_EXPORT_SPECIFIER  __declspec(dllexport)
+#define PJ_IMPORT_SPECIFIER  __declspec(dllimport)
+
+/*
+#define PJ_DLL  1
+#ifdef _LIB
+#	define PJ_EXPORTING 1
+#endif
+*/
